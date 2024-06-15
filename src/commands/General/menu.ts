41cd187db57e3d392d87a35e04a14b938b271dbd
Ares <89419093+arestosora@users.noreply.github.com>
2024-06-15 01:56:41 -0500

    feat: menu command

---
+++ src/commands/General/menu.ts
@@ -0,0 +1,26 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { ChatInputCommand, Command } from "@sapphire/framework";
+import { strings } from "../../shared/strings";
+import { MainEmbed } from "../../shared/main.embed";
+
+@ApplyOptions<Command.Options>({
+    name: strings.commands.menu.name,
+    description: strings.commands.menu.description
+})
+export class SendMenuCommand extends Command {
+    constructor(context: Command.LoaderContext, options: Command.Options) {
+        super(context, {
+            ...options
+        })
+    }
+
+    override registerApplicationCommands(registry: ChatInputCommand.Registry) {
+        registry.registerChatInputCommand((builder) =>
+            builder.setName(this.name).setDescription(this.description)
+        )
+    }
+
+    override async chatInputRun(interaction: ChatInputCommand.Interaction) {
+        return interaction.user.send({ embeds: [MainEmbed] })
+    }
+}
